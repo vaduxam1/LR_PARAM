683f63ccc6725920ee45230ac2fc06e5e2c67378
vaduxam1 <vaduxa.m1@gmail.com>
2018-03-09 19:40:20 +0300

    задержка, которая не приводит к торможению скроллинга

--- lr_lib/core/var/vars.py
+++ lr_lib/core/var/vars.py
@@ -265,7 +265,7 @@ var_bar_3 = False  # show/hide info bar
 
 HighlightOn = True  # включить подсветку
 HighlightAfter1 = 250  # задержка(мс), перед запуском подсветки всех линий на экране
-HighlightAfter2 = 100  # задержка(мс), перед запуском подсветки одной линии на экране
+HighlightAfter2 = 250  # задержка(мс), перед запуском подсветки одной линии на экране
 HighlightMPool = tk.BooleanVar(value=False)  # искать индексы для подсветки линий, в M_POOL
 HighlightLinesPortionSize = tk.IntVar(value=1)  # для скольки линий, искать индексы, за один проход/поток
 Background = 'khaki'
