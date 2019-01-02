47da087ad4421ce3d42bf4825a00d10b08fa7207
vaduxam1 <vaduxa.m1@gmail.com>
2019-01-02 21:22:45 +0300

    ref

--- lr_lib/gui/widj/wrsp_setting.py
+++ lr_lib/gui/widj/wrsp_setting.py
@@ -100,7 +100,7 @@ class WrspSettingWindow(tk.Toplevel):
                               command=lambda: self.parent.save_action_file(file_name=False))
         wrsp_rename_btn = tk.Button(
             self, font='Arial 7', text='wrsp_rename',
-            command=lambda *_: lr_lib.core_gui.rename.all_wrsp_auto_rename(parent=self),
+            command=lambda *_: lr_lib.core_gui.rename.all_wrsp_rename(self.parent, parent=self),
         )
         wrsp_auto_rename_btn = tk.Button(
             _lab, font='Arial 8 bold italic', text='wrsp_auto_rename',
