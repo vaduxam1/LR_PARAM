9d4627676230baca2c3a831d0fef28681b57ed9e
vaduxam1 <vaduxa.m1@gmail.com>
2019-01-28 19:35:06 +0300

    ref

--- lr_lib/gui/etc/git_update.py
+++ lr_lib/gui/etc/git_update.py
@@ -6,24 +6,6 @@ import tkinter.messagebox
 import urllib.request
 
 import lr_lib.core.var.vars as lr_vars
-# import lr_lib.gui.wrsp.main_window
-
-
-# def init(c_args=None) -> None:
-#     """создать gui"""
-#     print(c_args)
-#     lr_vars.Window = lr_lib.gui.wrsp.main_window.Window()  # main Gui
-#
-#     return
-#
-#
-# def start(action=True, lock=True) -> None:
-#     """action + lock"""
-#     if action:  # action Gui
-#         lr_vars.Window.new_action_window()  # lr_lib.gui.action.main_action.ActionWindow()
-#     if lock:  # main thread lock
-#         lr_vars.Tk.mainloop()
-#     return
 
 
 def _git_update_check():
