c6181468d2b697f30b4d70f8aff792a1d8890ad6
vaduxam1 <vaduxa.m1@gmail.com>
2019-03-06 16:21:20 +0300

    ref

--- lr_start.cmd
+++ lr_start.cmd
@@ -1,16 +1,17 @@
+rem v11.6.5 __main__
 @echo off
 set NLS_LANG=RUSSIAN_RUSSIA.CL8MSWIN1251
 
-assoc | findstr /i /r ".py=" > Null
+rem // проверить наличие установленного python, по windows-ассоциации с файлом: ".py=Python.File"
+assoc | findstr /i /r ".py=Python" > NUL
 
 if %errorlevel% == 0 (
-    echo lr_start.py
-    lr_start.py
+    echo python already installed
 
 ) ELSE (
-    echo install python-3.4.4
+    echo installing python-3.4.4
     lr_lib\whl\python-3.4.4.msi /quiet TargetDir=c:\Python34\ AssociateFiles=1
-
-    echo 2: lr_start.py
-    lr_start.py
 )
+
+echo run lr_start.py ...
+lr_start.py
