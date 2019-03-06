428771cec38b90d8e941ea2ab69d7a2ccd733253
vaduxam1 <vaduxa.m1@gmail.com>
2019-03-06 23:25:07 +0300

    ref

--- lr_start.cmd
+++ lr_start.cmd
@@ -1,9 +1,8 @@
-rem v11.6.5 __main__
 @echo off
+rem v11.6.5 __main__
 set NLS_LANG=RUSSIAN_RUSSIA.CL8MSWIN1251
 
 rem проверка установлен ли python
-assoc | findstr /i /r ".py=Python.File" > NUL
 python /? > NUL
 if not %errorlevel% == 0 (
     c:\Python34\python.exe /? > NUL
@@ -11,10 +10,10 @@ if not %errorlevel% == 0 (
 
 if %errorlevel% == 0 (
     echo python already installed
-
 ) ELSE (
     echo installing python-3.4.4
     lr_lib\whl\python-3.4.4.msi /quiet TargetDir=c:\Python34\ AssociateFiles=1
+
     rem Просмотр и изменение типов файлов, сопоставленных с расширением имен файлов
     ftype Python.File=c:\Python34\python.exe
     rem Просмотр и изменение сопоставлений файлов.
