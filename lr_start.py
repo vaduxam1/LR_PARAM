aa2fbde6486e57a5fca1dea20fea1f6ebd7c96e6
vaduxam1 <vaduxa.m1@gmail.com>
2019-08-16 17:42:55 +0300

    fix v11.6.8

--- lr_start.py
+++ lr_start.py
@@ -1,10 +1,33 @@
 ﻿# -*- coding: UTF-8 -*-
 # v11.6.5 __main__
 
+import os
 import sys
 
 from lr_lib.etc.excepthook import full_tb_write
 import lr_lib.whl.whl_installer
+
+
+def set_from_arg(start_py: 'sys.argv[0]') -> str:
+    """
+    Задать рабочий каталог программы, из полного имени главного запускаемого файла.py, например из sys.argv[0]
+    :param start_py: str: 'E:/SCR/proc_ruler/start.py'
+    :return: str: 'E:/SCR/proc_ruler'
+    Если запускаем скрипт как windows-службу, необходимо сменить домашнюю директорию "окружения", до импорта всех
+            пользовательских модулей, на директорию запускаемого скрипта, тк будет стоять директория python.exe.
+    Запуск скрипта, как windows-службы, с использованием nssm-2.24:
+        Создать: .\nssm-2.24\win64\nssm.exe install vaduxa_process_ruler C:\python37\python.exe E:\proc_ruler\start.py
+        Удалить: .\nssm-2.24\win64\nssm.exe remove vaduxa_process_ruler
+    """
+    (folder, _file) = os.path.split(start_py)  # ('E:/SCR/proc_ruler', 'start.py')
+    disk = folder[:3]  # 'C:/'
+    # Если текущая домашняя директория, находится на диске('C:/'), отличном от диска задаваемой директории('E:/'):
+    os.chdir(disk)  # Сначала необходимо сменить диск.
+    os.chdir(folder)  # А уже затем менять домашнюю директорию. Можно и в обратном порядке.
+    return folder
+
+
+set_from_arg(sys.argv[0])  # задать рабочий каталог
 
 
 def main() -> int:
